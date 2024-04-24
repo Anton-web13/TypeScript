64fcc01fd1917518b4918b8930f0e7d47e9e24c5
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2024-04-24 16:06:59 +0400

    Lesson 011 type

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ let revenue = 1000;
 let bonus = 500;
 let res = revenue + bonus;
 console.log(res);
+//-------------------------011----------------------------
+// function getFullName(firstname: string, surname: string): string {
+//     return `${firstname} ${surname}`;
+// };
+// const getFullNameArrow = (firstname: string, surname: string): string => {
+//     return `${firstname} ${surname}`;
+// };
+// console.log(getFullName('zsrfsdgf', 'zdgrfxdgd'));
+// console.log(getFullNameArrow('234234', '23544567'));
+function getFullNameBoolean(firstname, surname) {
+    return `${firstname} ${surname}`;
+}
+;
+console.log(getFullNameBoolean(true, false));
--- index.ts
+++ index.ts
@@ -22,3 +22,30 @@ let bonus: number = 500;
 let res: number = revenue + bonus;
 
 console.log(res);
+
+
+//-------------------------011----------------------------
+
+
+// function getFullName(firstname: string, surname: string): string {
+//     return `${firstname} ${surname}`;
+// };
+
+// const getFullNameArrow = (firstname: string, surname: string): string => {
+//     return `${firstname} ${surname}`;
+// };
+
+// console.log(getFullName('zsrfsdgf', 'zdgrfxdgd'));
+// console.log(getFullNameArrow('234234', '23544567'));
+
+
+
+
+function getFullNameBoolean(firstname: boolean, surname: boolean) {
+    return `${firstname} ${surname}`;
+};
+
+console.log(getFullNameBoolean(true, false));
+
+
+
